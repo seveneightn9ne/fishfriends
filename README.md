661725e4b22b7d8ed6d37b07f5b7b6b3f2339001
Miles Steele <miles@milessteele.com>
2016-07-23 22:35:45 -0400

    two way fishies

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ A virtual fishtank for our friends' fish.
 ## Running
 ```shell
 $ webpack-dev-server --inline  # Starts client server at localhost:8080
+$ google-chrome http://localhost:8080/webpack-dev-server/index.html
 ```
--- client.js
+++ client.js
@@ -13,10 +13,11 @@ var fishes = []
 PIXI.loader.add('fish', 'images/fish.png').load(function (loader, resources) {
 	for (var i = 0; i < 8; i++) {
 		var fish = new PIXI.Sprite(resources.fish.texture)
+		let dir = (i % 2 == 0) ? 1 : -1
 		fishes.push(fish)
 		fish.position.x = 400
 		fish.position.y = 300
-		fish.scale.x = .1
+		fish.scale.x = .1 * -dir
 		fish.scale.y = .1
 		stage.addChild(fish)
 	}
@@ -27,13 +28,23 @@ PIXI.loader.add('fish', 'images/fish.png').load(function (loader, resources) {
 function animLoop() {
 	requestAnimationFrame(animLoop)
 	fishes.forEach((fish, i) => {
-		fish.position.x -= 3 + Math.sin(i)
-		if (fish.position.x <= -90) {
-			fish.position.x = renderer.width + 100
-		}
+		let dir = (i % 2 == 0) ? 1 : -1
+		let vel = 3 + Math.sin(i)
+		fish.position.x += vel * dir
+		fish.position.x = wrapLimits(-90, renderer.width + 90, fish.position.x)
 		let cycle = Math.sin(Date.now() / 500. + i * 0.8)
 		fish.position.y = (-200 + 50 * i) + renderer.height / 2 + cycle * 30
 		fish.rotation = cycle / 5.
 	})
 	renderer.render(stage)
+}
+
+function wrapLimits(min, max, x) {
+	if (x < min) {
+		return max
+	} else if (x > max) {
+		return min
+	} else {
+		return x
+	}
 }
