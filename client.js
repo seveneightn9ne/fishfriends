d26b4f37afd4832d1674f2c786af0babc69c5669
Miles Steele <miles@milessteele.com>
2016-07-23 22:28:07 -0400

    more fishies

--- client.js
+++ client.js
@@ -8,26 +8,32 @@ document.body.appendChild(renderer.view)
 
 var stage = new PIXI.Container()
 
-var fish = null
+var fishes = []
 
 PIXI.loader.add('fish', 'images/fish.png').load(function (loader, resources) {
-	fish = new PIXI.Sprite(resources.fish.texture)
-	fish.position.x = 400
-	fish.position.y = 300
-	fish.scale.x = .1
-	fish.scale.y = .1
-	stage.addChild(fish)
+	for (var i = 0; i < 8; i++) {
+		var fish = new PIXI.Sprite(resources.fish.texture)
+		fishes.push(fish)
+		fish.position.x = 400
+		fish.position.y = 300
+		fish.scale.x = .1
+		fish.scale.y = .1
+		stage.addChild(fish)
+	}
+
 	animLoop()
 })
 
 function animLoop() {
 	requestAnimationFrame(animLoop)
-	fish.position.x -= 3
-	if (fish.position.x <= 0) {
-		fish.position.x = renderer.width
-	}
-	var cycle = Math.sin(Date.now() / 500.)
-	fish.position.y = renderer.height / 2 + cycle * 30
-	fish.rotation = cycle / 5.
+	fishes.forEach((fish, i) => {
+		fish.position.x -= 3 + Math.sin(i)
+		if (fish.position.x <= -90) {
+			fish.position.x = renderer.width + 100
+		}
+		let cycle = Math.sin(Date.now() / 500. + i * 0.8)
+		fish.position.y = (-200 + 50 * i) + renderer.height / 2 + cycle * 30
+		fish.rotation = cycle / 5.
+	})
 	renderer.render(stage)
 }
